0838242b0237e7fca3cde6cdb12a52800aec55db
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-08-29 21:16:44 +0100

    refactoring react app to nextjs

---
+++ components/SocialList.tsx
@@ -0,0 +1,40 @@
+const info = {
+	items: [
+		{
+			href: "https://github.com/JolomiTee",
+			ion_icon: "logo-github",
+			title: "Github",
+		},
+		{
+			href: "https://www.linkedin.com/in/jolomitee",
+			ion_icon: "logo-linkedin",
+			title: "LinkedIn",
+		},
+		{
+			href: "https://x.com/jolomitee",
+			ion_icon: "logo-twitter",
+			title: "X (formerly Twitter)",
+		},
+	],
+};
+
+const SocialList = () => {
+	return (
+		<ul className="social-list flex justify-start items-center gap-4 pb-1 ps-2 2xl:justify-center">
+			{info.items.map((items, index) => (
+				<li key={index} className="social-item">
+					<a
+						href={items.href}
+						target="_blank"
+						title={items.title}
+						className="social-link text-light-gray-70 text-fs-2 hover:text-light-gray"
+					>
+						{/* <ion-icon name={items.ion_icon}></ion-icon> */}
+					</a>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default SocialList;
