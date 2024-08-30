4e81679d007a8ad4d9a6571a0663cc1aff3f1261
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-08-30 17:59:07 +0100

    - styles adjustment

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,16 +1,20 @@
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
 import { projects } from "@/lib/data";
 import { Eye } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 function splitStr(str: string, separator: string) {
 	const categoryArray = str.split(separator);
 	return categoryArray.map((item, index) => (
-		<span
+		<Badge
+			variant={"outline"}
 			key={index}
-			className="tag border border-solid border-white-2 px-3 rounded-xl"
+			className="text-vegas-gold border border-white-2 px-3 rounded-xl text-pretty capitalize"
 		>
 			{item}
-		</span>
+		</Badge>
 	));
 }
 
@@ -28,21 +32,19 @@ const Page = () => {
 				<ul className="project-list flex justify-center flex-wrap gap-8 mb-3">
 					{projectList.map((project, index) => (
 						<li key={index} className="project-item block">
-							<a className="w-full" target="_blank" href={project.link}>
+							<Link className="w-full" target="_blank" href={project.link}>
 								<figure className="project-img relative  aspect-video rounded-2xl overflow-hidden mb-4 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-opacity-0 z-[1] transition-1">
 									{project.link !== "" ? (
-										<div className="project-item-icon-box scale-.75 bg-jet text-orange-yellow-crayola absolute top-[35%] left-[40%] text-[20px] p-5 rounded-xl opacity-0 z-[1] transition-1">
+										<div className="project-item-icon-box scale-.75">
 											<Eye />
 										</div>
 									) : (
-										<span
-											style={{
-												backgroundColor: "hsl(0, 43%, 51%)",
-											}}
-											className="text-[12px] text-white-1 absolute top-2 right-2 rounded-full px-3 py-1 flex justify-center items-center"
+										<Badge
+											variant={"destructive"}
+											className="absolute top-2 right-2 "
 										>
 											Awaiting deployment
-										</span>
+										</Badge>
 									)}
 
 									<Image
@@ -56,12 +58,11 @@ const Page = () => {
 								<h3 className="project-title ml-3 mb-2 text-white-1 text-fs-4 font-fw-400 capitalize leading-[1.3]">
 									{project.name}
 								</h3>
-
-								<div className="tags ml-3 flex flex-wrap gap-2 text-vegas-gold text-fs-7">
-									{splitStr(project.category, comma)}
-								</div>
-							</a>
-							<div className="separator md:my-6"></div>
+							</Link>
+							<div className="ml-3 flex flex-wrap gap-2 text-fs-5">
+								{splitStr(project.category, comma)}
+							</div>
+							<Separator className="my-5" />
 						</li>
 					))}
 				</ul>
