88755f1792f8230d0de75e2e758303f26c53f746
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-08-31 03:17:49 +0100

    - adding optional filtering components to projects - yet to implement filtering logic

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,6 +9,55 @@ import Link from "next/link";
 import { useState } from "react";
 
 import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from "@/components/ui/command";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+
+const tags = [
+	{
+		value: "next.js",
+		label: "Next.js",
+	},
+	{
+		value: "sveltekit",
+		label: "SvelteKit",
+	},
+	{
+		value: "nuxt.js",
+		label: "Nuxt.js",
+	},
+	{
+		value: "remix",
+		label: "Remix",
+	},
+	{
+		value: "astro",
+		label: "Astro",
+	},
+];
 
 function splitStr(str: string, separator: string) {
 	const categoryArray = str.split(separator);
@@ -28,6 +77,8 @@ let comma = ",";
 const Page = () => {
 	const [search, setSearch] = useState("");
 	const [filteredProject, setFilteredProject] = useState(projects);
+	const [open, setOpen] = useState(false);
+	const [value, setValue] = useState("");
 
 	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSearch(event.target.value); // Update the search state with the current input value
@@ -44,13 +95,81 @@ const Page = () => {
 				<h2 className="h2 main-title mb-6">My Projects</h2>
 			</header>
 
-			<div className="mt-5 mb-7">
+			<div className="mt-5 mb-7 flex items-center justify-between gap-5">
 				<Input
 					placeholder="Search project"
 					value={search}
 					className="w-1/3 bg-transparent text-white"
 					onChange={(e) => handleSearch(e)}
 				/>
+
+				<div className="hidden xl:flex items-center gap-5">
+					<div className="flex items-center space-x-2">
+						<Switch id="more-filters" />
+						<Label htmlFor="more-filters" className="text-light-gray">
+							More filters
+						</Label>
+					</div>
+
+					<div className="flex justify-between gap-5 items-center">
+						<Popover open={open} onOpenChange={setOpen}>
+							<PopoverTrigger asChild>
+								<Button
+									variant="outline"
+									role="combobox"
+									aria-expanded={open}
+									className="w-[150px] justify-between"
+								>
+									{value
+										? tags.find((tags) => tags.value === value)?.label
+										: "Select Tag..."}
+									<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+								</Button>
+							</PopoverTrigger>
+							<PopoverContent className="w-[150px] p-0">
+								<Command>
+									<CommandInput placeholder="Search Tags..." />
+									<CommandList>
+										<CommandEmpty>No tag found.</CommandEmpty>
+										<CommandGroup>
+											{tags.map((tags) => (
+												<CommandItem
+													key={tags.value}
+													value={tags.value}
+													onSelect={(currentValue) => {
+														setValue(
+															currentValue === value ? "" : currentValue
+														);
+														setOpen(false);
+													}}
+												>
+													<Check
+														className={cn(
+															"mr-2 h-4 w-4",
+															value === tags.value ? "opacity-100" : "opacity-0"
+														)}
+													/>
+													{tags.label}
+												</CommandItem>
+											))}
+										</CommandGroup>
+									</CommandList>
+								</Command>
+							</PopoverContent>
+						</Popover>
+
+						<Select>
+							<SelectTrigger className="w-[180px]">
+								<SelectValue placeholder="Category" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectItem value="system">All</SelectItem>
+								<SelectItem value="light">Frontend</SelectItem>
+								<SelectItem value="dark">Backend</SelectItem>
+							</SelectContent>
+						</Select>
+					</div>
+				</div>
 			</div>
 
 			<section className="projects">
