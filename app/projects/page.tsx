0716cced8e0a3fcd80a8bcb8d0f1713806b3f180
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-08-31 02:07:27 +0100

    - added basic filtering to projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import { projects } from "@/lib/data";
 import { Eye } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
+import { Input } from "@/components/ui/input";
 
 function splitStr(str: string, separator: string) {
 	const categoryArray = str.split(separator);
@@ -19,18 +24,38 @@ function splitStr(str: string, separator: string) {
 }
 
 let comma = ",";
-const projectList = projects.reverse();
 
 const Page = () => {
+	const [search, setSearch] = useState("");
+	const [filteredProject, setFilteredProject] = useState(projects);
+
+	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearch(event.target.value); // Update the search state with the current input value
+		const searchValue = event.target.value.toLowerCase(); // Convert the input value to lowercase
+		const filtered = projects.filter(
+			(project) => project.name.toLowerCase().includes(searchValue) // Check if the project name includes the search term
+		);
+		setFilteredProject(filtered.reverse()); // Update the filtered posts state with the filtered projects
+	};
+
 	return (
 		<>
 			<header>
 				<h2 className="h2 main-title mb-6">My Projects</h2>
 			</header>
 
+			<div className="mt-5 mb-7">
+				<Input
+					placeholder="Search project"
+					value={search}
+					className="w-1/3 bg-transparent text-white"
+					onChange={(e) => handleSearch(e)}
+				/>
+			</div>
+
 			<section className="projects">
 				<ul className="project-list flex justify-center flex-wrap gap-8 mb-3">
-					{projectList.map((project, index) => (
+					{filteredProject.map((project, index) => (
 						<li key={index} className="project-item block">
 							<Link className="w-full" target="_blank" href={project.link}>
 								<figure className="project-img relative  aspect-video rounded-2xl overflow-hidden mb-4 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-opacity-0 z-[1] transition-1">
